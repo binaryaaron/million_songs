e7abae665fc95a41dbb5ea7adce013f330163f29
Chris  E Ottino <cottino@galles.alliance.unm.edu>
2015-11-25 09:53:46 -0700

    version used to calculate the nextSong set

--- CFRecommender.java
+++ CFRecommender.java
@@ -1,5 +1,5 @@
 import java.io.*; 
-import java.util.*; 
+import java.util.*;; 
 import org.apache.mahout.cf.taste.impl.model.file.*;
 import org.apache.mahout.cf.taste.impl.neighborhood.*;
 import org.apache.mahout.cf.taste.impl.recommender.*;
@@ -11,7 +11,7 @@ import org.apache.mahout.cf.taste.similarity.*;
 import org.apache.mahout.cf.taste.impl.model.AbstractDataModel.*;
 import org.apache.mahout.cf.taste.impl.model.MemoryIDMigrator; 
 import org.apache.mahout.cf.taste.impl.model.*; 
-import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.*;
 import org.apache.mahout.cf.taste.impl.common.*;
 import java.util.Map.Entry; 
 import org.apache.mahout.cf.taste.impl.model.GenericUserPreferenceArray; 
@@ -21,11 +21,7 @@ import org.apache.mahout.cf.taste.impl.eval.*;
 import org.apache.mahout.common.RandomUtils; 
 
 /**
-*Basic implementation of a collaborative filter recommender. There are a couple of issues to be aware of 
-* 1. It is not quite finished and is untested. 
-* 2. The alpha-numeric string that represents the user sometimes ends up being negative when I convert to a float. I need to look into ways to fix this. 
-* 3. She talked about normalizing the data since some users will listen to a song alot and not like it as much as somebody who listens once.  I am not sure how to go about this, but will look into it. 
-*    
+*Basic implementation of a collaborative filter recommender. 
 **/
 class CFRecommender {
 
@@ -33,10 +29,11 @@ class CFRecommender {
     private MemoryIDMigrator memConverter = new MemoryIDMigrator(); 
     private UserSimilarity similarity; 
     private UserNeighborhood neighborhood = null; 
-    //  private Recommender recommender = null; 
     private  CachingRecommender recommender = null;  
     private int neighborRange = 15; 
-      
+    private FastByIDMap<PreferenceArray> fastPrefMap; 
+    private Map<Long,List<Preference>> allPrefs; 
+
     public static void main(String[] args) throws IOException, TasteException, UnsupportedEncodingException{
 
 	CFRecommender cfr = new CFRecommender(); 
@@ -45,12 +42,13 @@ class CFRecommender {
 	dataModel = cfr.createModel();
 	System.out.println("model has been created"); 
 	//build the caching recommender from the genericuser model. 
-	//cfr.recommender = cfr.initializeRecommender(dataModel);
-	cfr.calculateRSME(dataModel); 
+       	cfr.recommender = cfr.initializeRecommender(dataModel);
+        cfr.makeRecommendingSet(cfr.recommender); 
+	//cfr.calculateRSME(dataModel); 
     }
 
     /**
-     * Method for trying to calculate the RMSE. 
+     * Method for calculating the RMSE. 
      **/
     private void calculateRSME(DataModel model) throws TasteException, FileNotFoundException, UnsupportedEncodingException{
 	
@@ -66,7 +64,6 @@ class CFRecommender {
 	System.out.print("rmse is: "); 
 	System.out.println(rmse); 
 	PrintWriter writer = new PrintWriter("output.txt", "UTF-8"); 
-	writer.println("finished at least "); 
 	writer.println(rmse);
 	writer.close(); 
 	
@@ -74,27 +71,41 @@ class CFRecommender {
     
 
     /**
-     *Get a list of recommendations for a specific user. (UNTESTED)  
+     *Get a list of recommendations for a specific user. 
      **/
-    private List<RecommendedItem>  makeRecommendations(GenericUserBasedRecommender recommender, long user, int n ) throws TasteException 
+    private List<RecommendedItem>  makeRecommendations(CachingRecommender recommender, long user, int n ) throws TasteException 
     {
 	List<RecommendedItem> recommendations = recommender.recommend(user, n);
 	return recommendations; 
     }
     
-    //Little debugging method. 
-    private void printSimilarities(UserSimilarity pSim) throws TasteException{
-		//	System.out.println(pSim.userSimilarity(5582627969861115877,2427962755229849655)); 
-	//System.out.println(pSim.userSimilarity(5582627969861115877,9214985604270756226)); 
-	//	System.out.println(pSim.userSimilarity(1,2)); 
+    /**
+     *Make a set of users next song recommendation so we can have a start point for content based 
+     *recommendation. 
+     **/
+    private void  makeRecommendingSet(CachingRecommender r) throws TasteException, UnsupportedEncodingException, FileNotFoundException{
+	File file = new File("NextSongs.csv"); 
+	PrintWriter writer = new PrintWriter(file); 
+	
+	for(Entry<Long, List<Preference>> entry : allPrefs.entrySet()){
+	    System.out.println("Loop"); 
+	    long id = entry.getKey();
+	    List<RecommendedItem> nextList = r.recommend(id,1);
+	    if(nextList.size() > 0){
+		RecommendedItem nextSong = nextList.get(0); 
+		writer.println(memConverter.toStringID(id) + "," + nextSong.getItemID()); 
+	    }else
+		writer.println(memConverter.toStringID(id) + ",null" ); 
+	}
+	writer.close(); 
     }
-
-    /**Make sure that by passing it as a DataModel and not GenericDataModel that it still works, Maybe try to get this working better with different kinds of correlations.  
-     *
+    
+    /**
+     *Method to calculate the similarity of the model and the neighborhood to be used. 
      **/
     private CachingRecommender initializeRecommender(DataModel model) throws TasteException{
-	//similarity = new PearsonCorrelationSimilarity(model); 
-	similarity = new SpearmanCorrelationSimilarity(model); 
+	similarity = new PearsonCorrelationSimilarity(model); 
+	//similarity = new SpearmanCorrelationSimilarity(model); 
 	neighborhood = new NearestNUserNeighborhood(neighborRange, similarity, model);
 	GenericUserBasedRecommender gr = new GenericUserBasedRecommender(model, neighborhood, similarity);
 	return new CachingRecommender(gr); 
@@ -107,7 +118,7 @@ class CFRecommender {
     private GenericDataModel createModel() throws FileNotFoundException, IOException{
 	File train_triplets = new File("training.csv"); //add your path.
 	CSVParser parser = new CSVParser(new InputStreamReader(new FileInputStream(train_triplets))); 
-	Map<Long,List<Preference>> allPrefs = new HashMap<Long, List<Preference>>(); 
+        allPrefs = new HashMap<Long, List<Preference>>(); 
 	
 	String[] line; 
 	while((line = parser.getLine()) != null){
@@ -115,13 +126,12 @@ class CFRecommender {
 	    String songID = line[1]; 
 	    int playCount = Integer.parseInt(line[2]); 
 	    
-	    //Convert the two Strings for user and song to usable longs. Im doing this because the fileDataModel doesn't work for our data.  
-	    long userLong = memConverter.toLongID(userID); //Coming out negative alot ? 
-	    long songLong = memConverter.toLongID(songID); 
+	    //    System.out.println(userID); 
+	    long userLong = memConverter.toLongID(userID);
+	    memConverter.storeMapping(userLong, userID); 
+       	    long songLong = memConverter.toLongID(songID);
+	    memConverter.storeMapping(songLong, songID); 
 	    
-	    //Print out the userLong just to see what they are. 
-	    //	    System.out.println(userLong); 
-
 	    List<Preference> userPrefs; 
       	    if((userPrefs = allPrefs.get(userLong)) == null){
 	    	userPrefs = new ArrayList<Preference>();  
@@ -130,11 +140,12 @@ class CFRecommender {
 	    userPrefs.add(new GenericPreference(userLong, songLong, playCount)); 
 	}
 
-	FastByIDMap<PreferenceArray> fastPrefMap = new FastByIDMap<PreferenceArray>(); 
+	fastPrefMap = new FastByIDMap<PreferenceArray>(); 
 	for(Entry<Long, List<Preference>> entry : allPrefs.entrySet()){
             convertToPercentages(entry.getValue());
             fastPrefMap.put(entry.getKey(), new GenericUserPreferenceArray(entry.getValue())); 
 	}
+	
 	return new GenericDataModel(fastPrefMap);   
     }
 
